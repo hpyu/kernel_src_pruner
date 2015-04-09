0b89e8bc637d4dea62452ab8c718b24e233a30e5
YU Haipeng <hpyu@marvell.com>
2015-04-09 17:58:11 +0800

    rewrite and works well 	TO: 	1, optimze command line parameter 	2, add full link option, and re-compile option 	3, integrate compiling stript 	4, more elegant param passing

--- make_source_list.py
+++ make_source_list.py
@@ -1,19 +1,8 @@
-#!/usr/bin/env python3.2
-
-import os,sys,shutil
-
-opened_source = {}
-opened_source2 = {}
-source_list = []
-
-if len(sys.argv) < 5:
-	print("Usage %s opened_files.txt kernel_dir link_dir source_list" % sys.argv[0])
-	sys.exit()
-
-strace_log = sys.argv[1]
-srcpath = os.path.realpath(sys.argv[2])
-dstpath = os.path.normpath(sys.argv[3])
-source_list_name = sys.argv[4]
+'''
+	[hpyu] 2015.4.8
+	Re-struct the code, remove global varibles
+	time python3.2 make_source_list.py ~/ramtmp/kernel/strace_log.txt ~/ramtmp/kernel ~/ramtmp/k
+'''
 
 def save_list_to_file(filename, listname):
 	f = open(filename,'w')
@@ -21,90 +10,127 @@ def save_list_to_file(filename, listname):
 		f.writelines(name)
 		f.writelines("\n")
 	f.close()
+
+def dump_to_files(file_map, opened_files):
+	source_list = []
+
+	l = list(file_map.keys())
+	l.sort()
+	save_list_to_file("file_map.txt", l)
+	l2 = list(opened_files.keys())
+	l2.sort()
+	save_list_to_file("opened_files.txt", l2)
+
+	for name in opened_files.keys():
+		if name[-2:] in ['.c', '.S', '.h']:
+			source_list.append(name)
+	source_list.sort()
+	save_list_to_file("source_list.txt", source_list)
+	
+def copy_source_tree(srcroot, dstroot):
+	for root, dirs, files in os.walk(srcroot,topdown=true, followlinks=true):
+		relpath = root[len(srcroot)+1:]
+		#print(dirs)
+		dirname = join(dstroot,relpath)
+		if not exists(dirname):
+			os.makedirs(dirname)
 		
-def make_opened_source():
-	global opened_source
-	global opened_source2
+		for name in files:
+			linkname = join(dstroot,relpath,name)
+			srcname = join(root, name)
+			if not exists(linkname):
+				os.symlink(srcname, linkname)
+
+def extract_fname(line, srcroot):
+	line_list = line.split("\"")
+	if len(line_list) > 2:
+		fname = line_list[1]
+		if fname[0] != '/' or fname[0:len(srcroot)] == srcroot:
+			if fname[0:len(srcroot)] == srcroot:
+				fname = fname[len(srcroot)+1:]
+
+			if exists(os.path.join(srcroot, fname)) and \
+				fname[-2:] not in ['.o', '.d'] and \
+				fname[-4:] not in ['.cmd', '.tmp']:
+				return fname
+	return ""
+
+def extract_opened_files(strace_log, opened_files, srcroot):
+	file_map = {}
 
 	try:
 		f = open(strace_log, 'r')
 		for line in f.readlines():
-			line_list = line.split(" ")
-			if line_list[1][:4] == "open" and '-1' not in line_list:
-				fname = line.split("\"")[1]
-				if fname[0] != '/' or fname[0:len(srcpath)] == srcpath:
-					if fname[0:len(srcpath)] == srcpath:
-						fname = fname[len(srcpath)+1:]
+			if " -1 " not in line:
+				name = extract_fname(line, srcroot)
+				file_map.setdefault(name,True)
 
-					if fname[-2:] not in ['.o', '.d'] and \
-						fname[-4:] not in ['.cmd', '.tmp']:
-						opened_source.setdefault(fname,True)
-		
-		for name in opened_source.keys():
+		for name in file_map.keys():
 			if name.find('..') != -1:
-				opened_source2.setdefault(os.path.normal(name),True)
+				opened_files.setdefault(normpath(name),True)
 			else:
-				opened_source2.setdefault(name,True)
+				opened_files.setdefault(name,True)
 
-		l = list(opened_source.keys())
-		l.sort()
-		save_list_to_file("opened_source.txt", l)
-		l2 = list(opened_source2.keys())
-		l2.sort()
-		save_list_to_file("opened_source_2.txt", l2)
-		for name in opened_source2.keys():
-			if name[-2:] in ['.c', '.S', '.h']:
-				source_list.append(name)
-		source_list.sort()
-		save_list_to_file(source_list_name, source_list)
+		dump_to_files(file_map, opened_files)
 
-	except IOError:
+	except IOError as e:
+		printf(e)
 		sys.exit()
 
-
-def make_list_and_linkdir():
-	if os.path.exists(dstpath):
-		print("remove "+dstpath)
-		shutil.rmtree(dstpath)
+def build_clean_tree(opened_files, srcroot, dstroot):
+	if exists(dstroot):
+		shutil.rmtree(dstroot)
 	
-	os.makedirs(dstpath, mode=0o777)
-
-	for root, dirs, files in os.walk(srcpath,topdown=True, followlinks=True):
-		relpath = root[len(srcpath)+1:]
-		#print("root: "+root)
-		#print(dirs)
-		dirname = os.path.join(dstpath,relpath)
-		if not os.path.exists(dirname):
-			os.makedirs(dirname)
+	os.makedirs(dstroot, mode=0o777)
 		
-		for name in files:
-			linkname = os.path.join(dstpath,relpath,name)
-			srcname = os.path.join(root, name)
-			if not os.path.exists(linkname):
-				os.symlink(srcname, linkname)
+	for name in opened_files.keys():
+		src = join(srcroot, name)
+		dst = join(dstroot, name)
 
-	for root, dirs, files in os.walk(dstpath,topdown=True):
-		for name in files:
-			relpath = os.path.join(root[len(dstpath)+1:], name)
-			if relpath[-2:] in ['.c', '.h', '.S']:
-				if relpath not in opened_source2:
-					rmname =  os.path.join(root, name)
-					#print("remove: "+rmname)
-					os.remove(rmname)
+		if not exists(os.path.dirname(dst)):
+			os.makedirs(dirname(dst), mode=0o777)
+		#	os.mkdir(dst, mode=0o777, dir_fd=None)
+
+		if islink(src):
+			printf(src + " is a link")
+
+	for name in opened_files.keys():
+		src = join(srcroot, name)
+		dst = join(dstroot, name)
+
+		if isfile(src) and not os.path.exists(dst):
+			shutil.copyfile(src, dst)
+			shutil.copymode(src, dst)
 			
+
+def main():
+	opened_files = {}
+
+	if len(sys.argv) < 4:
+		printf("Usage: %s strace_log src_dir dst_dir" % sys.argv[0])
+		sys.exit()
+
+	strace_log = sys.argv[1]
+	srcroot = normpath(sys.argv[2])
+	dstroot = normpath(sys.argv[3])
 	
-def copy_missed_files():
-	#hz.h is just called by stat(), and only this files is missed
-	#so handle it here to reduce time
-	missed_files = ['include/config/hz.h']		
-	for name in missed_files:
-		sname = os.path.join(srcpath, name)
-		lname = os.path.join(dstpath, name)
-		os.symlink(sname, lname)
-		
+	if exists(dstroot):
+		printf("remove "+dstroot)
+		rm = input(dstroot + " exists, enter Y if you agree to remove:")
+		if rm in ['y', 'Y']:
+			shutil.rmtree(dstroot)
+		else:
+			printf("Exit because not agree to remove " + dstroot)
+			sys.exit()
 
-make_opened_source()
-make_list_and_linkdir()
-copy_missed_files()
+	os.makedirs(dstroot, mode=0o777)
+	extract_opened_files(strace_log, opened_files, srcroot)
+	build_clean_tree(opened_files, srcroot, dstroot)
 
+if __name__ == '__main__':
+	# Python2.x & 3.x compatible
+	from distutils.log import warn as printf
+	from os.path import *
+	import os,sys,shutil
+	main()
 
